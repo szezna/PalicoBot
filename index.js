248b0204a929189ce7dc380a178d39a4cfc246ae
Veqaz <pascal.wd@icloud.com>
2020-06-20 13:00:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ bot.on('message', msg => {
             break;
         case 'roll':
             min = Math.ceil(1);
-            max = Math.floor(1);
+            max = Math.floor(110);
             var number = Math.floor(Math.random() * (max - min + 1)) + min;
 
             if (number > 0 && number < 49) {
@@ -215,110 +215,107 @@ bot.on("messageReactionRemove", async (reaction, user) => {
 
 const monster = new Map([
     // C-Tier 48
-    [1, 'https://i.imgur.com/A56DdR3.png'],  // | Anjanath
+    [1, 'https://i.imgur.com/HRcm2na.png'],  // | Anjanath
     [2, 'https://i.imgur.com/cfjKUA3.png'],  // | Arzuros
-    [3, 'https://imgur.com/sKbGSXv'],  // | Pukei Pukei
-    [4, 'https://imgur.com/MG3p0Hm'],  // | Qurupeco
-    [5, 'https://imgur.com/Oteqxbb'],  // | Rathalos
-    [6, 'https://imgur.com/1gR3iCY'],  // | Rathian
-    [7, 'https://imgur.com/ZPuaeCG'],  // | Royal Ludroth
-    [8, 'https://imgur.com/6zEVxlO'],  // | Ruby Basarios
-    [9, 'https://imgur.com/9ghtUAh'],  // | Shogun Ceanataur
-    [10, 'https://imgur.com/eYZnM4t'], // | Tigrex
-    [11, 'https://imgur.com/sS9DqGj'], // | Tobi Kadachi
-    [12, 'https://imgur.com/TkgFYEU'], // | Uragaan
-    [13, 'https://imgur.com/yWc2in1'], // | Volvidon
-    [14, 'https://imgur.com/5OSW64N'], // | Yian Garuga
-    [15, 'https://imgur.com/0Fdljc7'], // | Barioth
-    [16, 'https://imgur.com/xkFU8Kf'], // | Diablos
-    [17, 'https://imgur.com/lH8gOND'], // | Congalala
-    [18, 'https://imgur.com/3hSiBMe'], // | Yian Kut Ku
-    [19, 'https://imgur.com/qAK3rmC'], // | Daimyo Hermitaur
-    [20, 'https://imgur.com/0NGoeji'], // | Zamtrios
-    [21, 'https://imgur.com/8zPVtqa'], // | Dodogama
-    [22, 'https://imgur.com/KucJJzw'], // | Barroth
-    [23, 'https://imgur.com/1Ej64Cs'], // | Basarios
-    [24, 'https://imgur.com/kaXrMv0'], // | Blangonga
-    [25, 'https://imgur.com/zPH2rjM'], // | Cephadrome
-    [26, 'https://imgur.com/XWldpPH'], // | Emerald Congalala
-    [27, 'https://imgur.com/vQ9Q2xE'], // | Gravios
-    [28, 'https://imgur.com/yx4KKwm'], // | Gigginox
-    [29, 'https://imgur.com/wGvFJr2'], // | Great Girros
-    [30, 'https://imgur.com/4lGN909'], // | Great Baggi
-    [31, 'https://imgur.com/vR9tYvz'], // | Gobul
-    [32, 'https://imgur.com/bARH8be'], // | Great Jaggi
-    [33, 'https://imgur.com/iN4l5jG'], // | Green Plesioth
-    [34, 'https://imgur.com/87hJOhY'], // | Gypceros
-    [35, 'https://imgur.com/asjhyte'], // | Jyuratodos
-    [36, 'https://imgur.com/iSwlnX3'], // | Jade Barroth
-    [37, 'https://imgur.com/Id7lguf'], // | Kecha Wacha
-    [38, 'https://imgur.com/uotwQne'], // | Lavasioth
-    [39, 'https://imgur.com/HiUOyXI'], // | Lagombi
-    [40, 'https://imgur.com/weKzd2o'], // | Khezu
-    [41, 'https://imgur.com/xcFPkR1'], // | Legiana
-    [42, 'https://imgur.com/QAt9xZ5'], // | Malfestio
-    [43, 'https://imgur.com/qIhkeVD'], // | Nerscylla
-    [44, 'https://imgur.com/RNamIO8'], // | Nargacuga
-    [45, 'https://imgur.com/gCMMNc2'], // | Nibelsnarf
-    [46, 'https://imgur.com/nkkXTyB'], // | Odogaron
-    [47, 'https://imgur.com/JFhGR07'], // | Paolumu
-    [48, 'https://imgur.com/NbSXHx0'], // | Plesioth
-
+    [3, 'https://i.imgur.com/sKbGSXv.png'],  // | Pukei Pukei
+    [4, 'https://i.imgur.com/MG3p0Hm.png'],  // | Qurupeco
+    [5, 'https://i.imgur.com/Oteqxbb.png'],  // | Rathalos
+    [6, 'https://i.imgur.com/1gR3iCY.png'],  // | Rathian
+    [7, 'https://i.imgur.com/ZPuaeCG.png'],  // | Royal Ludroth
+    [8, 'https://i.imgur.com/6zEVxlO.png'],  // | Ruby Basarios
+    [9, 'https://i.imgur.com/9ghtUAh.png'],  // | Shogun Ceanataur
+    [10, 'https://i.imgur.com/eYZnM4t.png'], // | Tigrex
+    [11, 'https://i.imgur.com/sS9DqGj.png'], // | Tobi Kadachi
+    [12, 'https://i.imgur.com/TkgFYEU.png'], // | Uragaan
+    [13, 'https://i.imgur.com/yWc2in1.png'], // | Volvidon
+    [14, 'https://i.imgur.com/5OSW64N.png'], // | Yian Garuga
+    [15, 'https://i.imgur.com/0Fdljc7.png'], // | Barioth
+    [16, 'https://i.imgur.com/xkFU8Kf.png'], // | Diablos
+    [17, 'https://i.imgur.com/lH8gOND.png'], // | Congalala
+    [18, 'https://i.imgur.com/3hSiBMe.png'], // | Yian Kut Ku
+    [19, 'https://i.imgur.com/qAK3rmC.png'], // | Daimyo Hermitaur
+    [20, 'https://i.imgur.com/0NGoeji.png'], // | Zamtrios
+    [21, 'https://i.imgur.com/8zPVtqa.png'], // | Dodogama
+    [22, 'https://i.imgur.com/KucJJzw.png'], // | Barroth
+    [23, 'https://i.imgur.com/1Ej64Cs.png'], // | Basarios
+    [24, 'https://i.imgur.com/kaXrMv0.png'], // | Blangonga
+    [25, 'https://i.imgur.com/zPH2rjM.png'], // | Cephadrome
+    [26, 'https://i.imgur.com/XWldpPH.png'], // | Emerald Congalala
+    [27, 'https://i.imgur.com/vQ9Q2xE.png'], // | Gravios
+    [28, 'https://i.imgur.com/yx4KKwm.png'], // | Gigginox
+    [29, 'https://i.imgur.com/wGvFJr2.png'], // | Great Girros
+    [30, 'https://i.imgur.com/4lGN909.png'], // | Great Baggi
+    [31, 'https://i.imgur.com/vR9tYvz.png'], // | Gobul
+    [32, 'https://i.imgur.com/bARH8be.png'], // | Great Jaggi
+    [33, 'https://i.imgur.com/iN4l5jG.png'], // | Green Plesioth
+    [34, 'https://i.imgur.com/87hJOhY.png'], // | Gypceros
+    [35, 'https://i.imgur.com/asjhyte.png'], // | Jyuratodos
+    [36, 'https://i.imgur.com/iSwlnX3.png'], // | Jade Barroth
+    [37, 'https://i.imgur.com/Id7lguf.png'], // | Kecha Wacha
+    [38, 'https://i.imgur.com/uotwQne.png'], // | Lavasioth
+    [39, 'https://i.imgur.com/HiUOyXI.png'], // | Lagombi
+    [40, 'https://i.imgur.com/weKzd2o.png'], // | Khezu
+    [41, 'https://i.imgur.com/xcFPkR1.png'], // | Legiana
+    [42, 'https://i.imgur.com/QAt9xZ5.png'], // | Malfestio
+    [43, 'https://i.imgur.com/qIhkeVD.png'], // | Nerscylla
+    [44, 'https://i.imgur.com/RNamIO8.png'], // | Nargacuga
+    [45, 'https://i.imgur.com/gCMMNc2.png'], // | Nibelsnarf
+    [46, 'https://i.imgur.com/nkkXTyB.png'], // | Odogaron
+    [47, 'https://i.imgur.com/JFhGR07.png'], // | Paolumu
+    [48, 'https://i.imgur.com/NbSXHx0.png'], // | Plesioth
     // B-Tier 33
-    [49, 'https://imgur.com/I159Nbq'], // | Agnaktor
-    [50, 'https://imgur.com/Kym9jlM'], // | Astalos
-    [51, 'https://imgur.com/q7CzwJb'], // | Atlal - Ka
-    [52, 'https://imgur.com/PtMDRxB'], // | Azure Rathalos
-    [53, 'https://imgur.com/FmPWuD2'], // | Bazelgeuse
-    [54, 'https://imgur.com/hCOQFsb'], // | Black Diablos
-    [55, 'https://imgur.com/pwqG3Tt'], // | Gammoth
-    [56, 'https://imgur.com/POjSnJU'], // | Brachydios
-    [57, 'https://imgur.com/GkmVPTu'], // | Brute Tigrex
-    [58, 'https://imgur.com/9rv9IKM'], // | Chameleos
-    [59, 'https://imgur.com/qwoCY3C'], // | Deviljho
-    [60, 'https://imgur.com/3dydhwP'], // | Glacial Agnaktor
-    [61, 'https://imgur.com/tx9bmLd'], // | Glavenus
-    [62, 'https://imgur.com/aksvSvD'], // | Gold Rathian
-    [63, 'https://imgur.com/NeIG5Z3'], // | Gore Magala
-    [64, 'https://imgur.com/4FqkUzn'], // | Ivory Lagiacrus
-    [65, 'https://imgur.com/qYBypHv'], // | Kirin
-    [66, 'https://imgur.com/EWTcBzr'], // | Kushala Daora
-    [67, 'https://imgur.com/B463JYe'], // | Lagiacrus
-    [68, 'https://imgur.com/KPGd831'], // | Lunastra
-    [69, 'https://imgur.com/Ufx23GR'], // | Mizutsune
-    [70, 'https://imgur.com/rDwaJFq'], // | Molten Tigrex
-    [71, 'https://imgur.com/WtVZ72Z'], // | Oroshi Kirin
-    [72, 'https://imgur.com/Rbw6Z0N'], // | Pink Rathian
-    [73, 'https://imgur.com/dnzzgNj'], // | Rajang
-    [74, 'https://imgur.com/b3v4a5l'], // | Sand Barioth
-    [75, 'https://imgur.com/bx14irj'], // | Seregios
-    [76, 'https://imgur.com/sah7HYe'], // | Shen Gaoren
-    [77, 'https://imgur.com/fB7aNHt'], // | Silver Rathalos
-    [78, 'https://imgur.com/BIUb7DU'], // | Teostra
-    [79, 'https://imgur.com/mVYQPVr'], // | Vaal Hazak
-    [80, 'https://imgur.com/GrXuCfL'], // | White Monoblos
-    [81, 'https://imgur.com/90wlEWX'], // | Zinogre
-
+    [49, 'https://i.imgur.com/I159Nbq.png'], // | Agnaktor
+    [50, 'https://i.imgur.com/Kym9jlM.png'], // | Astalos
+    [51, 'https://i.imgur.com/q7CzwJb.png'], // | Atlal - Ka
+    [52, 'https://i.imgur.com/PtMDRxB.png'], // | Azure Rathalos
+    [53, 'https://i.imgur.com/FmPWuD2.png'], // | Bazelgeuse
+    [54, 'https://i.imgur.com/hCOQFsb.png'], // | Black Diablos
+    [55, 'https://i.imgur.com/pwqG3Tt.png'], // | Gammoth
+    [56, 'https://i.imgur.com/POjSnJU.png'], // | Brachydios
+    [57, 'https://i.imgur.com/GkmVPTu.png'], // | Brute Tigrex
+    [58, 'https://i.imgur.com/9rv9IKM.png'], // | Chameleos
+    [59, 'https://i.imgur.com/qwoCY3C.png'], // | Deviljho
+    [60, 'https://i.imgur.com/3dydhwP.png'], // | Glacial Agnaktor
+    [61, 'https://i.imgur.com/tx9bmLd.png'], // | Glavenus
+    [62, 'https://i.imgur.com/aksvSvD.png'], // | Gold Rathian
+    [63, 'https://i.imgur.com/NeIG5Z3.png'], // | Gore Magala
+    [64, 'https://i.imgur.com/4FqkUzn.png'], // | Ivory Lagiacrus
+    [65, 'https://i.imgur.com/qYBypHv.png'], // | Kirin
+    [66, 'https://i.imgur.com/EWTcBzr.png'], // | Kushala Daora
+    [67, 'https://i.imgur.com/B463JYe.png'], // | Lagiacrus
+    [68, 'https://i.imgur.com/KPGd831.png'], // | Lunastra
+    [69, 'https://i.imgur.com/Ufx23GR.png'], // | Mizutsune
+    [70, 'https://i.imgur.com/rDwaJFq.png'], // | Molten Tigrex
+    [71, 'https://i.imgur.com/WtVZ72Z.png'], // | Oroshi Kirin
+    [72, 'https://i.imgur.com/Rbw6Z0N.png'], // | Pink Rathian
+    [73, 'https://i.imgur.com/dnzzgNj.png'], // | Rajang
+    [74, 'https://i.imgur.com/b3v4a5l.png'], // | Sand Barioth
+    [75, 'https://i.imgur.com/bx14irj.png'], // | Seregios
+    [76, 'https://i.imgur.com/sah7HYe.png'], // | Shen Gaoren
+    [77, 'https://i.imgur.com/fB7aNHt.png'], // | Silver Rathalos
+    [78, 'https://i.imgur.com/BIUb7DU.png'], // | Teostra
+    [79, 'https://i.imgur.com/mVYQPVr.png'], // | Vaal Hazak
+    [80, 'https://i.imgur.com/GrXuCfL.png'], // | White Monoblos
+    [81, 'https://i.imgur.com/90wlEWX.png'], // | Zinogre
     // A-Tier 18
-    [82, 'https://imgur.com/zALuAi7'], // | Akantor
-    [83, 'https://imgur.com/A56DdR3'], // | Ceadeus
-    [84, 'https://imgur.com/PXsuNP5'], // | Chaotic Gore Magala
-    [85, 'https://imgur.com/1hEG5c4'], // | Dah'ren Mohran
-    [86, 'https://imgur.com/mHpomds'], // | Furious Rajang
-    [87, 'https://imgur.com/Dd3TyXc'], // | Gogmazios
-    [88, 'https://imgur.com/QIJdLaQ'], // | Jhen Moran
-    [89, 'https://imgur.com/LzulcF5'], // |  Kulve Taroth
-    [90, 'https://imgur.com/sRgFSSj'], // | Lao - Shan Lung
-    [91, 'https://imgur.com/V4n8Ynu'], // | Nergigante
-    [92, 'https://imgur.com/WjLoFSE'], // | Rusted Kushala Daora
-    [93, 'https://imgur.com/R9qLdXX'], // | Savage Deviljho
-    [94, 'https://imgur.com/lrdO5IT'], // | Shagaru Magala
-    [95, 'https://imgur.com/b7vhRS6'], // | Stygian Zinogre
-    [96, 'https://imgur.com/LSHbN7B'], // | Ukanlos
-    [97, 'https://imgur.com/E74llnu'], // | Valstrax
-    [98, 'https://imgur.com/mqwDy5y'], // | Yama Tsukami
-    [99, 'https://imgur.com/nxmSIHf'], // | Zorah Magdaros
-
+    [82, 'https://i.imgur.com/zALuAi7.png'], // | Akantor
+    [83, 'https://i.imgur.com/A56DdR3.png'], // | Ceadeus
+    [84, 'https://i.imgur.com/PXsuNP5.png'], // | Chaotic Gore Magala
+    [85, 'https://i.imgur.com/1hEG5c4.png'], // | Dah'ren Mohran
+    [86, 'https://i.imgur.com/mHpomds.png'], // | Furious Rajang
+    [87, 'https://i.imgur.com/Dd3TyXc.png'], // | Gogmazios
+    [88, 'https://i.imgur.com/QIJdLaQ.png'], // | Jhen Moran
+    [89, 'https://i.imgur.com/LzulcF5.png'], // |  Kulve Taroth
+    [90, 'https://i.imgur.com/sRgFSSj.png'], // | Lao - Shan Lung
+    [91, 'https://i.imgur.com/V4n8Ynu.png'], // | Nergigante
+    [92, 'https://i.imgur.com/WjLoFSE.png'], // | Rusted Kushala Daora
+    [93, 'https://i.imgur.com/R9qLdXX.png'], // | Savage Deviljho
+    [94, 'https://i.imgur.com/lrdO5IT.png'], // | Shagaru Magala
+    [95, 'https://i.imgur.com/b7vhRS6.png'], // | Stygian Zinogre
+    [96, 'https://i.imgur.com/LSHbN7B.png'], // | Ukanlos
+    [97, 'https://i.imgur.com/E74llnu.png'], // | Valstrax
+    [98, 'https://i.imgur.com/mqwDy5y.png'], // | Yama Tsukami
+    [99, 'https://i.imgur.com/nxmSIHf.png'], // | Zorah Magdaros
     // S-Tier 11
     [100, 'https://i.imgur.com/jDqbzyr.png'], // | Alatreon
     [101, 'https://i.imgur.com/N8duTTi.png'], // | Amatsu
