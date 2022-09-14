f6d88490d4c06827c3491d4c18771674c5bba43e
Veqaz <33842693+Veqaz@users.noreply.github.com>
2022-09-14 20:33:20 +0200

    Delete moretesting.js

--- commands/moretesting.js
+++
@@ -1,7 +0,0 @@
-const functions = require('../functions.js');
-
-module.exports.run = (bot, msg) => {
-    if (msg.author.id === '231474317051953155') {
-    msg.reply('random number: ' + functions.roll(0, 150) + '\nHello: ' + functions.hello());
-    }
-}
